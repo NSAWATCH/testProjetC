867d525e137d8e933af5cc8891469280d56d14d2
Lucas <nsawatch12@gmail.com>
2015-02-17 23:34:58 +0100

    rapides changements..

--- projetGitHub/main.c
+++ projetGitHub/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LIGNE printf("\n");
+
 int main()
 {
     printf("Hello world!\n");
-    printf("Fort bien les enfants !! ");
+    printf("Fort bien les enfants !!");
+    LIGNE LIGNE
+    printf("#pasTropDIdeesLaToutDeSuite....");
+    LIGNE
     return 0;
 }
